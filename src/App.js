12a52f8489b729268d7796a958511a89cb9dde76
razielll <asto1387@gmail.com>
2020-01-30 12:35:35 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+      restaurants: [],
+      isLoaded: false,
+      error: ''
+    }
+  }
+
+  componentDidMount() {
+  }
+
+  handleSumibt = (e) => {
+    e.preventDefault()
+    if (!this.state.searchQuery) return;
+    console.log('handl sumbit');
+    this.fetchResults();
+  };
+
+  fetchResults = () => {
+    const API_URL = 'http://opentable.herokuapp.com/api/restaurants?city='
+
+    fetch(API_URL + this.state.searchQuery)
+      .then(res => res.json())
+      .then((result) => {
+        console.log('GOT RESULT', result)
+        this.setState({
+          isLoaded: true,
+          restaurants: result.restaurants
+        });
+      },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchQuery: e.target.value })
+  }
+
+  render() {
+    const { searchQuery } = this.state;
+    return (
+      <>
+        <h1>hi</h1>
+        <form onSubmit={this.handleSumibt}>
+          <input type="text" value={searchQuery} onChange={this.handleChange} />
+        </form>
+
+        {this.state.restaurants && this.state.restaurants.length && this.state.restaurants.map((restaurant, i) => (
+          <div key={restaurant.id}>
+            {restaurant.name} {restaurant.address} {restaurant.price}
+          </div>
+        )
+        )}
+      </>
+    )
+  }
+}
